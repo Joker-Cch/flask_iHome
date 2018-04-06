7beba22f361a820b4d2fb43c18e3d323d23e277f
Joker <Joker_Cch@163.com>
2018-04-06 14:29:43 +0800

    配置信息、脚本管理器、Mysql与Redis配置

--- manage.py
+++ manage.py
@@ -1,6 +1,45 @@
+# -*- coding:utf-8 -*-
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import redis
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+class Config(object):
+    """加载配置"""
+
+    # 开启调试模式
+    DEBUG = True
+
+    # 配置mysql数据库:开发中使用真实IP
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.244.128:3306/flask_iHome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置redis数据库：开发中使用真实IP
+    REDIS_HOST = '192.168.244.128'
+    REDIS_PORT = 6379
+
 
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
+
+# 创建连接到mysql数据库的对象
+db = SQLAlchemy(app)
+
+# 创建连接到redis数据库的对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 创建脚本管理器对象
+manager = Manager(app)
+
+# 让app和db在迁移时建立关联
+Migrate(app, db)
+# 将数据库迁移脚本添加到甲苯管理器
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
@@ -9,4 +48,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
